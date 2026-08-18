64b9705d60e81b2341a6a18f639c8b01b4afb93c
suwi <suwi.1226@gmail.com>
2026-08-19 00:34:02 +0800

    Level-0 : Rename, Greet, Exit

---
+++ src/main/java/Bucket.java
@@ -0,0 +1,16 @@
+public class Bucket {
+    public static void main(String[] args) {
+        String banner = " ____   _   _   ____  _  __ _____  _____ \n"
+                + "| __ ) | | | | / ___|| |/ /| ____||_   _|\n"
+                + "|  _ \\ | | | || |    | ' / |  _|    | |  \n"
+                + "| |_) || |_| || |___ | . \\ | |___   | |  \n"
+                + "|____/  \\___/  \\____||_|\\_\\|_____|  |_|  \n";
+        String lineBreak = "-------------------------";
+        System.out.println(lineBreak);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Bucket\nYou again?");
+        System.out.println(lineBreak);
+        System.out.println("BYE!");
+        System.out.println(lineBreak);
+    }
+}
