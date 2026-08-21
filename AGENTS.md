4622cf07336ca6be888762058f2348ed7ed96227
suwi <suwi.1226@gmail.com>
2026-08-21 13:30:42 +0800

    Level 1: echo

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [Occasionally good]
-* IDE and level of expertise: [Decent]
+* Level of programming experience: [Occasionally good to horrendous]
+* IDE and level of expertise: [Decent - clicking buttons]
 
 # Guidance for interacting with users
 
--- src/main/java/Bucket.java
+++ src/main/java/Bucket.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bucket {
     public static void main(String[] args) {
         String banner = " ____   _   _   ____  _  __ _____  _____ \n"
@@ -10,6 +12,17 @@ public class Bucket {
         System.out.println(banner);
         System.out.println("Hello! I'm Bucket\nYou again?");
         System.out.println(lineBreak);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equalsIgnoreCase("bye")) {
+            System.out.println(lineBreak);
+            System.out.println(input);
+            System.out.println(lineBreak);
+            input = scanner.nextLine();
+        }
+        
         System.out.println("BYE!");
         System.out.println(lineBreak);
     }
