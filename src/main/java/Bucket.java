5742fbd315551985b60616a9f6a6cfe3db2a77a3
suwi <suwi.1226@gmail.com>
2026-08-21 13:42:36 +0800

    Level 2: add list

--- src/main/java/Bucket.java
+++ src/main/java/Bucket.java
@@ -8,6 +8,9 @@ public class Bucket {
                 + "| |_) || |_| || |___ | . \\ | |___   | |  \n"
                 + "|____/  \\___/  \\____||_|\\_\\|_____|  |_|  \n";
         String lineBreak = "-------------------------";
+        String[] items = new String[100];
+        int index = 0;
+
         System.out.println(lineBreak);
         System.out.println(banner);
         System.out.println("Hello! I'm Bucket\nYou again?");
@@ -17,12 +20,18 @@ public class Bucket {
         String input = scanner.nextLine();
 
         while (!input.equalsIgnoreCase("bye")) {
+            items[index] = input;
+            index++;
+
             System.out.println(lineBreak);
-            System.out.println(input);
+            for (int i = 0; i < index; i++) {
+                System.out.println((i + 1) + ". " + items[i]);
+            }
             System.out.println(lineBreak);
             input = scanner.nextLine();
         }
         
+        scanner.close();
         System.out.println("BYE!");
         System.out.println(lineBreak);
     }
